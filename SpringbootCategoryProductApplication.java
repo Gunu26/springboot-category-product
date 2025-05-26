7a754c9a7cae74064c8a5ec9ff7a92948f6f5b02
Gunvanti_Bendale <138374124+Gunu26@users.noreply.github.com>
2025-05-26 17:18:43 +0530

    Add SpringBootCategoryProductApplication main class.
    * Initialize Spring Boot application entry point, 
    * Configure SpringBootCategoryProductApplication as project bootstrap class, 
    * Add @SpringBootApplication annotation to main class, 
    * Refactor main application class for clarity.

---
+++ SpringbootCategoryProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_category_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCategoryProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCategoryProductApplication.class, args);
+	}
+
+}
